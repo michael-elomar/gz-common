e5ba4845a078dc293d8f86bc569fe36672db939b
Michael El Omar <michaelelomar@outlook.com>
2026-04-05 22:47:56 +0200

    add atom.mk

---
+++ atom.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH := $(call my-dir)
+
+################################################################################
+# gz-common
+################################################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := gz-common
+LOCAL_DESCRIPTION := An audio-visual library supports processing audio and video files, a graphics library can load a variety 3D mesh file formats into a generic in-memory representation, and the core library of Ignition Common contains functionality that spans Base64 encoding/decoding to thread pools
+
+LOCAL_LIBRARIES := \
+	gz-cmake \
+	gz-math \
+	gz-utils \
+	tinyxml2 \
+	assimp \
+	gdal \
+	freeimage
+
+LOCAL_CMAKE_CONFIGURE_ARGS := \
+	-DBUILD_TESTING:BOOL=False \
+	-DFREEIMAGE_RUNS=1 \
+	-DFREEIMAGE_RUNS__TRYRUN_OUTPUT=""
+
+LOCAL_CMAKE_CONFIGURE_ENV +=\
+	PKG_CONFIG_PATH=$(TARGET_OUT_STAGING)/usr/lib/pkgconfig:$(TARGET_OUT_STAGING)/usr/share/pkgconfig
+
+
+# LOCAL_EXPORT_C_INCLUDES := $(TARGET_OUT_STAGING)/usr/include/ignition/common3
+
+# LOCAL_EXPORT_LDLIBS := -lignition-common3
+
+include $(BUILD_CMAKE)
+
